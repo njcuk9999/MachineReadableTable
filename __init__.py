ff7c46ccd96cb72c963d60681c29e8b4d4dfd652
Neil Cook <neil.james.cook@gmail.com>
2017-10-10 18:22:10 -0400

    Added init file

---
+++ __init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 2017-10-06 at 13:29
+
+@author: cook
+
+
+
+Version 0.0.0
+"""
+
+from . import core
+
+
+MRT = core.MachineReadableTable
